3484044b6d889c52e3d4f30bbf31fd8fa57ff84f
eunjye <eunjh1220@naver.com>
2023-08-07 22:55:19 +0900

    훅 리팩토링

--- src/hook/useAsset.js
+++ src/hook/useAsset.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 import useAuth from './useAuth';
 
 const useAsset = () => {
-  const { userId } = useAuth();
+  const { userId } = useAuth(); // 필요한 경우 useAuth 훅을 가져옵니다.
   const setAssetData = useSetRecoilState(assetState);
 
   const handleAsset = async () => {
     try {
-      const res = await axios.get('http://localhost:3000/users', { params: { username: userId } });
+      const res = await axios.get('http://localhost:3002/users', { params: { username: userId } });
       const userData = res.data[0];
       const assets = userData.assets;
       setAssetData(assets);
@@ -19,9 +19,25 @@ const useAsset = () => {
       console.error(`asset error ::${err}`);
     }
   };
+  
+
+  const deleteAsset = async ({ id: assetId }) => {
+    try {
+      const res = await axios.get('http://localhost:3002/users', { params: { username: userId } });
+      const userData = res.data[0];
+      const updateAssets = userData.assets.filter((item) => item.id !== assetId);
+
+      await axios.put('http://localhost:3002/users', { ...userData, assets: updateAssets });
+
+      setAssetData(updateAssets);
+    } catch (err) {
+      console.log(`asset delete error :: ${err}`);
+    }
+  };
+  
 
   
-  return { setAssetData, handleAsset };
+  return { handleAsset, deleteAsset };
 };
 
 export default useAsset;
--- src/hook/useAuth.js
+++ src/hook/useAuth.js
@@ -9,32 +9,32 @@ const useAuth = () => {
   const navigate = useNavigate();
 
   const handleLogin = async (userId, userPw) => {
-    const state = await axios.get('http://localhost:3000/users', { params: { username: userId, password: userPw }})
-      .then((res) => {
-        const user = res.data[0];
-        if (user) {
-          setIsLoggedIn(true);
-          setAtomUserId(userId);
-          localStorage.setItem('token', 'loggedIn');
-          return true;
-        }
-        alert('아이디나 패스워드를 확인하세요.');
+    try {
+      const res = await axios.get('http://localhost:3002/users', { params: { username: userId, password: userPw }});
+      const user = res.data[0];
+      if (user) {
+        setIsLoggedIn(true);
+        setAtomUserId(userId);
+        localStorage.setItem('token', 'loggedIn');
+        return true;
+      } else {
+        alert('아이디나 패스워드를 확인해주세요.');
         return false;
-      })
-      .catch((err)=> {
-        console.error(`login error ::${err}`);
-      });
-
-    return state;
+      }
+    } catch (err) {
+      console.error(`login error ::${err}`);
+      alert('로그인 중 오류가 발생했습니다. 다시 시도해주세요.');
+      return false;
+    }
   };
 
   const handleLogout = () => {
     setIsLoggedIn(false);
     localStorage.removeItem('token');
     navigate('/');
-  }
+  };
 
-  return { isLoggedIn, setIsLoggedIn, handleLogin, handleLogout };
+  return { isLoggedIn, handleLogin, handleLogout };
 };
 
 export default useAuth;
